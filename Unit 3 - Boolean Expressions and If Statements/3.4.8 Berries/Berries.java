0b527ab259f3ddc056e771caae91ea0566f1ca16
GingerSnap-I <charzar.aka.rj@gmail.com>
2024-10-19 16:36:12 -0700

    2024 Update Berries.java

--- Unit 3 - Boolean Expressions and If Statements/3.4.8 Berries/Berries.java
+++ Unit 3 - Boolean Expressions and If Statements/3.4.8 Berries/Berries.java
@@ -4,28 +4,16 @@ public class Berries
 {
     public static void main(String[] args)
     {
-        // Ask for a berry initial
         System.out.println("Enter the initial of the berry: ");
         Scanner scan = new Scanner(System.in);
         char berry = scan.next().charAt(0);
-        
-        // To get the input as a character, use the String method
-        // charAt().  Use str.charAt(0) since you want the
-        // first character
-        
-        
-        // Now you can compare characters using ==
-        
-        // Use comments to list the different
-        // branches you will need before you write the code
-        
-        if (berry == 'r') {
-            System.out.println("You ordered raspberry");
-        } else if (berry == 'h') {
-            System.out.println("You ordered huckleberry");
-        } else if (berry == 'g') {
-            System.out.println("You ordered goji berry");
-        } else {
+        if(berry == 'E'){
+            System.out.println("You ordered Elderberry");
+        }else if(berry == 'L'){
+            System.out.println("You ordered Lingonberry");
+        }else if(berry == 'G'){
+            System.out.println("You ordered Gooseberry");
+        }else{
             System.out.println("Berry not recognized");
         }
     }
