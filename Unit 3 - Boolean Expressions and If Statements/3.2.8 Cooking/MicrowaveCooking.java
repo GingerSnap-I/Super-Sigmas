aaa8f81183510c808b865e811e7a09700771aca3
GingerSnap-I <charzar.aka.rj@gmail.com>
2024-10-19 16:25:48 -0700

    2024MicrowaveCooking.java

--- Unit 3 - Boolean Expressions and If Statements/3.2.8 Cooking/MicrowaveCooking.java
+++ Unit 3 - Boolean Expressions and If Statements/3.2.8 Cooking/MicrowaveCooking.java
@@ -2,19 +2,14 @@ public class MicrowaveCooking
 {
     public static void main(String[] args)
     {
-       // Generate a random number of seconds
-       // between 0 and 60
-       int number = (int) Math.round(Math.random() * 60);
-       
-       // Print the number of seconds
+       int number = (int) Math.round((Math.random() * 150)+90);
+
        System.out.println("Microwaving for " + number + " seconds");
        
-       // Use two if statements to print
-       // whether the roll is fine or will catch fire
-       if (number > 20) {
-           System.out.println("Your roll will catch fire!");
+       if (number > 120) {
+           System.out.println("Rolls will be boiling hot!");
        } else {
-           System.out.println("Perfect cooking time!");
+           System.out.println("Rolls will be the right temperature!");
        }
     }
 }
