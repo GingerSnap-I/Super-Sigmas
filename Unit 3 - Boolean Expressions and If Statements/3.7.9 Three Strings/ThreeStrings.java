fe6bee66f238bf5607d0b590ef7453169f7d4570
GingerSnap-I <charzar.aka.rj@gmail.com>
2024-10-20 16:26:15 -0700

    Update ThreeStrings.java

--- Unit 3 - Boolean Expressions and If Statements/3.7.9 Three Strings/ThreeStrings.java
+++ Unit 3 - Boolean Expressions and If Statements/3.7.9 Three Strings/ThreeStrings.java
@@ -5,9 +5,8 @@ public class ThreeStrings
     public static void main(String[] args)
     {
         Scanner scan = new Scanner(System.in);
-        //System.out.print("Enter your password:");
+        
         String userPass = scan.nextLine();
-        //System.out.print("Enter the company's secret code: ");
         String userCode = scan.nextLine();
         String companyCode = "1298";
         String userTotal = userPass + userCode;
@@ -15,7 +14,7 @@ public class ThreeStrings
         if (equal) {
             System.out.println("Access granted");
         } else {
-            System.out.println(userTotal+" is denied");
+            System.out.println(userTotal + " is denied");
         }
     }
 }
