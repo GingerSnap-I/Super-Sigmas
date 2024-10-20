1b16b7792d16da00ad525137a87b90b3cf49b060
GingerSnap-I <charzar.aka.rj@gmail.com>
2024-10-20 16:23:58 -0700

    Update ThreeStrings.java

--- Unit 3 - Boolean Expressions and If Statements/3.7.9 Three Strings/ThreeStrings.java
+++ Unit 3 - Boolean Expressions and If Statements/3.7.9 Three Strings/ThreeStrings.java
@@ -1,26 +1,21 @@
 import java.util.Scanner;
+
 public class ThreeStrings
 {
     public static void main(String[] args)
     {
-        // Ask the user for three strings.
-        // Use a Boolean variable to test the comparison of 
-        // first+second equals third
-        // Remember since you are working with strings to 
-        // use equals() and NOT == !
         Scanner scan = new Scanner(System.in);
-        System.out.print("First string? ");
-        String first = scan.next();
-        System.out.print("Second string? ");
-        String second = scan.next();
-        System.out.print("Third string? ");
-        String third = scan.next();
-        String together = first + second;
-        boolean equal = together.equals(third);
+        //System.out.print("Enter your password:");
+        String userPass = scan.nextLine();
+        //System.out.print("Enter the company's secret code: ");
+        String userCode = scan.nextLine();
+        String companyCode = "1298";
+        String userTotal = userPass + userCode;
+        boolean equal = userTotal.equals(userPass+companyCode);
         if (equal) {
-            System.out.println(first + " + " + second + " is equal to " + third + "!");
+            System.out.println("Access granted");
         } else {
-            System.out.println(first + " + " + second + " is not equal to " + third + "!");
+            System.out.println(userTotal+" is denied");
         }
     }
 }
